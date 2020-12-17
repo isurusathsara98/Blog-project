82bb9f6bb9e1c8e8b852ab6e986bdd08c5ec6862
Isuru sathsara <isurusathsara183@gmail.com>
2020-12-17 19:15:29 +0530

    new

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require("express");
+const mysql = require("mysql");
+const dotenv = require("dotenv");
+
+dotenv.config({path : './.env'})
 
 const app = express();
+
+const db= mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password: '17/Eng/099',
+    database:'blog'
+})
+
+db.connect((error)=>{
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log("connected")
+    }
+})
 
 app.get("/",(req, res)=>{
     res.send("<h1>Home</h1>")
