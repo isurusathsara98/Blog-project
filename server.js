ca6608f9a5d208b7c71f3c41b517d49d29e04f73
isurusathsara98 <56884816+isurusathsara98@users.noreply.github.com>
2020-12-17 13:57:55 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+app.set('view-engine', 'ejs')
+
+app.get('/',(req, res)=>{
+    res.render('index.ejs', {name: 'Isuru'})
+})
+app.get('/login',(req, res)=>{
+    res.render('login.ejs', {name: 'Isuru'})
+})
+app.get('/register',(req, res)=>{
+    res.render('register.ejs', {name: 'Isuru'})
+})
+app.listen(3000)
