f7b7568f2abe8bc991cf89e4276511ff19e382b2
Isuru sathsara <isurusathsara183@gmail.com>
2020-12-17 23:01:39 +0530

    form_inputs

--- app.js
+++ app.js
@@ -28,10 +28,12 @@ db.connect((error)=>{
 const publicDirectory = path.join(__dirname,'./public');
 app.use(express.static(publicDirectory));
 
+app.use(express.urlencoded({ extended:false}));
+app.use(express.json());
 
-app.get("/",(req, res)=>{
-    res.render("index")
-});
+
+app.use('/',require('./routes/pages'));
+app.use('/auth',require('./routes/auth'));
 
 app.listen(3000,()=>{
     console.log("beautiful")
